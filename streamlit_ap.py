00738d07eebe9bfe0f3ecdb076a544e3b0844fd4
Abhishek Pal <165993270+AbhishekPal0736@users.noreply.github.com>
2024-07-14 20:16:47 +0530

    Update streamlit_ap.py

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score,classification_report
